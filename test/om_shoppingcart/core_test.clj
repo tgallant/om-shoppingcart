92719cf80a569d84eb0077a65d43c0cbe295a5e0
Tim Gallant <tim@privy.io>
2014-10-19 15:50:09 -0700

    init

---
+++ test/om_shoppingcart/core_test.clj
@@ -0,0 +1,7 @@
+(ns om-shoppingcart.core-test
+  (:require [clojure.test :refer :all]
+            [om-shoppingcart.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
