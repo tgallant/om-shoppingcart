8bb9c2b75f872b89874efd55dec873939f08c12a
Tim Gallant <me@timgallant.us>
2014-10-19 15:57:15 -0700

    init

--- resources/public/scripts/main.js
+++
@@ -1,6 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.string.StringBuffer', 'goog.array']);
-goog.addDependency("../om/dom.js", ['om.dom'], ['cljs.core']);
-goog.addDependency("../om/core.js", ['om.core'], ['cljs.core', 'om.dom', 'goog.ui.IdGenerator']);
-goog.addDependency("../clojure/string.js", ['clojure.string'], ['goog.string', 'cljs.core', 'goog.string.StringBuffer']);
-goog.addDependency("../om_shoppingcart/core.js", ['om_shoppingcart.core'], ['cljs.core', 'om.dom', 'om.core', 'clojure.string']);
--- resources/public/scripts/out/om_shoppingcart/core.cljs
+++
@@ -1,8 +0,0 @@
-(ns om-shoppingcart.core
-  (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]
-            [clojure.string :as string]))
-
-(enable-console-print!)
-
-(.log js/console "Hello!")
